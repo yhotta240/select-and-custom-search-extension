195241705569cf1da044930c100b101e17d373e1
yhotta240 <yhotta240@icloud.com>
2025-01-16 06:52:33 +0900

    ポップアップのテーマ切替機能を改善し，展開状態をローカルストレージに保存するように修正

--- popup/popup.js
+++ popup/popup.js
@@ -42,75 +42,103 @@ const defaultSites = [
 
 
 function customSearchPreview() {
-  chrome.storage.local.get(['sites', 'iconNum', 'theme'], (data) => {
+  chrome.storage.local.get(['sites', 'iconNum', 'theme', 'isExpanded'], (data) => {
     const sites = data.sites ?? defaultSites;
     const iconNum = data.iconNum ?? 8;
     const theme = data.theme ?? 'light';
+    const isExpanded = data.isExpanded ?? false;
+
     const changeTheme = document.querySelector('#change-theme');
-    changeTheme.checked = theme === 'dark';
     const changeThemeIcon = document.querySelector('#change-theme-icon');
-    const lightIcon = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
-      <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
-    </svg>
-    `;
 
-    const darkIcon = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
-      <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
-      <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
-    </svg>
-    `;
-    changeThemeIcon.innerHTML = theme === 'light' ? lightIcon : darkIcon;
+    changeTheme.checked = theme === 'dark';
 
-    const selBoxGroup = document.createElement('div');
+    const themeIcons = {
+      light: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+      </svg>
+      `,
+      dark: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+        <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
+        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+      </svg>
+      `
+    };
+
+    changeThemeIcon.innerHTML = themeIcons[theme];
+
+    const selBoxElement = document.createElement('div');
+    selBoxElement.className = 'my-extension-root sel-box-element';
     const gap = 2;
     const groupPadding = 6;
     const padding = 4;
     const buttonWidth = 20;
     const maxWidth = iconNum * (buttonWidth + (padding * 2) + gap) - gap + groupPadding * 2;
-
-    Object.assign(selBoxGroup.style, {
+    Object.assign(selBoxElement.style, {
       // position: "absolute",
-      backgroundColor: theme === 'light' ? '#ffffff' : '#292e33',
-      pointerEvents: 'absolute', // クリック可能にする
       maxWidth: `${maxWidth}px`,
       zIndex: 0,
     });
-    selBoxGroup.className = "btn-group1 my-extension-root flex-wrap1";
+
+    const selBoxGroup = document.createElement('div');
+    selBoxGroup.className = "btn-group1 my-extension-root flex-wrap1 w-100";
+    selBoxGroup.style.height = isExpanded
+      ? `${20 + (padding * 2) + gap + groupPadding}px`
+      : 'auto';
+    console.log("selBoxGroup.style.height", selBoxGroup.style.height);
+    selBoxGroup.style.backgroundColor = theme === 'light' ? '#ffffff' : '#292e33';
     selBoxGroup.role = "group";
-    selBoxGroup.ariaLabel = "アイコンリンクボタングループ";
-    selBoxGroup.id = "search-box"
+    selBoxGroup.ariaLabel = "search-box";
+    selBoxGroup.id = "search-box";
     chrome.storage.local.set({ sites: sites });
     sites.forEach((site, index) => {
+      if (index >= iconNum - 1 && isExpanded) return;
       const selBox = document.createElement("a");
-      // console.log("site", site);
       const iconUrl = getFaviconUrl(site.url);
       selBox.href = site.url;
       selBox.id = site.name;
       selBox.target = "_blank";
-      selBox.className = `btn1 selBoxGroup btn-icon1 m-auto1 ${theme === 'light' ? 'btn-light1' : 'btn-dark1'}`;
-      selBox.innerHTML = `<img src="${iconUrl}" alt="アイコン" style="width:20px; height:20px;">`;
+      selBox.className = `btn1 btn-icon1 m-auto1 ${theme === 'light' ? 'btn-light1' : 'btn-dark1'}`;
+      selBox.innerHTML = `<img src="${iconUrl}" alt="icon" style="width:20px; height:20px;">`;
       selBoxGroup.append(selBox);
     });
 
-    // #settings 要素を取得
     const previewElement = document.querySelector("#settings-preview");
     const offBtn = document.createElement('button');
     offBtn.className = `btn1 selBoxGroup btn-icon1 ${theme === 'light' ? 'btn-light1' : 'btn-dark1'}`;
     offBtn.style.width = '28px';
     offBtn.id = 'off-button';
-    const hiddenIcon = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban" viewBox="0 0 16 16">
-        <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
-      </svg>
+    const offIcon = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban" viewBox="0 0 16 16">
+    <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
+    </svg>
     `;
-    offBtn.innerHTML = hiddenIcon;
+    offBtn.innerHTML = offIcon;
     selBoxGroup.append(offBtn);
-    // 最初に挿入
-    previewElement.innerHTML = selBoxGroup.outerHTML;
-    listSites();
 
+    const expandBtn = document.createElement('button');
+    expandBtn.className = `expand-btn btn-sm rounded-bottom ${theme === 'light' ? 'btn-light1' : 'btn-dark1'} w-100 p-0 m-0`;
+    expandBtn.id = 'expand-button';
+    const expandIcon = {
+      expand: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
+      </svg>
+      `,
+      collapse: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+      </svg>
+      `
+    };
+    expandBtn.innerHTML = expandIcon[isExpanded ? 'expand' : 'collapse'];
+
+    selBoxElement.innerHTML = selBoxGroup.outerHTML + expandBtn.outerHTML;
+    previewElement.innerHTML = selBoxElement.outerHTML;
+
+    listSites();
   });
 }
 customSearchPreview();
@@ -123,6 +151,19 @@ changeThemeIcon.addEventListener('click', (event) => {
       customSearchPreview();
     });
   });
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.id === 'expand-button') {
+    chrome.storage.local.get(['isExpanded'], (data) => {
+      const isExpanded = data.isExpanded ?? false;
+      console.log('isExpanded', isExpanded);
+      chrome.storage.local.set({ isExpanded: !isExpanded }, () => {
+        customSearchPreview();
+      });
+    });
+  }
 });
 
 function listSites() {
@@ -294,7 +335,6 @@ document.getElementById("urlForm").addEventListener("submit", function (e) {
 // DOMの読み込み完了を監視し，完了後に実行
 document.addEventListener('DOMContentLoaded', function () {
 
-
   // アイコンの個数を設定
   const iconNumRange = document.querySelector('#icon-num-range');
   const iconNumText = document.querySelector('#icon-num-text');
@@ -304,7 +344,7 @@ document.addEventListener('DOMContentLoaded', function () {
     iconNumText.textContent = value;
     iconNumRange.value = value;
     chrome.storage.local.set({ iconNum: value }, () => {
-      customSearchPreview();
+      // customSearchPreview();
       messageOutput(dateTime(), `アイコンの個数：${value} `);
     });
   });
