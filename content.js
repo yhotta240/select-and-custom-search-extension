047406861cbd5b0d8ac31268c33f8f3a5e4f8ec2
yhotta240 <yhotta240@icloud.com>
2025-04-19 19:54:27 +0900

    offボタンにツールをOFFにするためのタイトルを追加

--- content.js
+++ content.js
@@ -200,6 +200,7 @@ function customSearch(selectedText) {
     offBtn.className = `btn1 selBoxGroup btn-icon1 ${newTheme === 'dark' ? 'btn-dark1' : 'btn-light1'}`;
     offBtn.style.width = '28px';
     offBtn.id = 'off-button';
+    offBtn.title = "ツールをOFFにする";
     const hiddenIcon = `
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban" viewBox="0 0 16 16">
         <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
